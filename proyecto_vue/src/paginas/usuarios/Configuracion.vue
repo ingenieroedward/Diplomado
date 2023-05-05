f5f3805768ce4f245499fa7a0b439852cbe0c5f6
Edward Diaz Padilla <edwarddiazpadilla@gmail.com>
2023-05-05 14:41:50 -0500

    actualizacion

--- proyecto_vue/src/paginas/usuarios/Configuracion.vue
+++ proyecto_vue/src/paginas/usuarios/Configuracion.vue
@@ -7,7 +7,13 @@ import Footer from '../../components/principal/Footer.vue'
     <article class=" py-5 w-full article flex flex-wrap justify-center">
         <section class="container w-full flex flex-wrap mx-auto justify-between   py-4 ">
             <h1 class="block w-full text-center mb-8">Configuración de la cuenta</h1>
-
+            <div id="exitoso" class="hidden flex w-full p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 " role="alert">
+                <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                <span class="sr-only">Info</span>
+                <div>
+                  <span class="font-medium">Cambio exitosot!</span> 
+                </div>
+              </div>
             <div class="mr-1 px-4 py-2 rounded-xl">
                 <h2 @click="showhidden('form1')"
                     class="cursor-pointer block w-80 text-center mb-4 px-4 py-2 rounded-xl bg-white">Datos de cuenta
@@ -279,6 +285,9 @@ export default {
 
             }).then(function (response) {
                 console.log(response.data);
+                document.getElementById('exitoso').classList.remove('hidden');
+                        setTimeout(() => { document.getElementById('ezitoso').classList.add('hidden') }, 3000)
+                        location.reload()
 
             }).catch(function (error) {
                 console.log(error)
@@ -304,6 +313,9 @@ export default {
 
                     }).then(function (response) {
                         console.log(response.data);
+                        document.getElementById('exitoso').classList.remove('hidden');
+                        setTimeout(() => { document.getElementById('ezitoso').classList.add('hidden') }, 3000)
+                        location.reload()
 
                     }).catch(function (error) {
                         console.log(error)
@@ -338,6 +350,9 @@ export default {
 
                 }).then(function (response) {
                     console.log(response.data);
+                    document.getElementById('exitoso').classList.remove('hidden');
+                        setTimeout(() => { document.getElementById('ezitoso').classList.add('hidden') }, 3000)
+                        location.reload()
 
                 }).catch(function (error) {
                     console.log(error)
